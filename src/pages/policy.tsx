6a5e65c087a4bace88e9f1f8536924da51bfa092
João Pedro Batista Santos <joaopbatistastos@gmail.com>
2023-08-29 15:33:59 -0300

    feat: policy page

---
+++ src/pages/policy.tsx
@@ -0,0 +1,151 @@
+import styles from "../styles/policy.module.scss";
+
+export default function Policy() {
+  return (
+    <>
+      <div className={styles.policyText}>
+        <p>Políticas de privacidade</p>
+        <p>
+          1. INFORMAÇÕES GERAIS E DEFINIÇÕES O Clube Benefit valoriza a proteção
+          dos dados pessoais dos Titulares (usuários) e este AVISO reafirma seu
+          compromisso em proteger a privacidade dos mesmos, em especial nos
+          termos da Lei 13.709/2018, Lei Geral de Proteção de Dados - LGPD, bem
+          como esclarecer a forma de tratamento adotada no tratamento de suas
+          informações. O Aviso de Privacidade A proteção e segurança dos seus
+          dados pessoais é muito importante para nós, por isso, esse Aviso de
+          Privacidade reafirma nosso compromisso com a Lei 13.709/2018, Lei
+          Geral de Proteção de Dados - LGPD, com proteger os seus dados, bem
+          como esclarece a forma adotada no tratamento das suas informações. Em
+          caso de dúvida entre em contato com o Encarregado de Dados por meio do
+          canal comercial@clubebenefit.com.br. <br />
+          <br /> 1.1 Definições: Titular: pessoa natural a quem se referem os
+          dados pessoais que são objeto de tratamento. Dados Pessoais: quaisquer
+          informações fornecidas e/ou coletadas pelo Clube Benefit, por qualquer
+          meio, que: (I) identifiquem, ou que, quando usadas em combinação com
+          outras informações tratadas, identifiquem um Titular. Os Dados
+          Pessoais podem estar em qualquer mídia ou formato, inclusive registros
+          eletrônicos. Os Dados Pessoais, no entanto, não incluem telefone,
+          número de celular e endereço que sejam comerciais. Dados Pessoais
+          Sensíveis: dado pessoal sobre origem racial ou étnica, convicção
+          religiosa, opinião política, filiação a sindicato ou a organização de
+          caráter religioso, filosófico ou político, dado referente à saúde ou à
+          vida sexual, dado genético ou biométrico, quando vinculado a uma
+          pessoa natural. Encarregado de Dados (DPO): pessoa indicada pelo
+          controlador e operador para atuar como canal de comunicação entre o
+          controlador, os titulares dos dados e a Autoridade Nacional de
+          Proteção de Dados (ANPD); Finalidade: o propósito que o Clube Benefit
+          deseja alcançar a partir de cada ato de tratamento das informações
+          pessoais. Necessidade: justificativa pelo qual é estritamente
+          necessário coletar dados pessoais, para atingir a finalidade. Bases
+          legais: fundamentação legal que torna legítimo o tratamento de dados
+          pessoais para uma determinada finalidade prévia por parte do Clube
+          Benefit. Consentimento: autorização expressa e inequívoca dada pelo
+          Titular do dado pessoal para que o Clube Benefit trate seus dados
+          pessoais para uma finalidade previamente descrita, na qual a base
+          legal necessária para o ato demande a autorização expressa do mesmo.
+          Tratamento: toda operação realizada com dados pessoais, como as que se
+          referem a coleta, produção, recepção, classificação, utilização,
+          acesso, reprodução, transmissão, distribuição, processamento,
+          arquivamento, armazenamento, eliminação, avaliação ou controle da
+          informação, modificação, comunicação, transferência, difusão ou
+          extração. Este AVISO faz referências aos procedimentos de coleta,
+          classificação, utilização, acesso, reprodução, transmissão,
+          distribuição, processamento, arquivamento e eliminação dos dados
+          pessoais que o Clube Benefit tem acesso, de acordo com as bases legais
+          aplicáveis e todas as leis de privacidade e proteção de dados em
+          vigor. <br />
+          <br /> 2. COLETA E USO DOS DADOS PESSOAIS O Titular concorda em
+          fornecer seus dados pessoais de forma consciente e voluntária por meio
+          de formulário, ou por meio do uso dos sites operados pelo Clube
+          Benefit e tem ciência de que serão mantidos em sigilo e utilizados
+          apenas para o propósito que motivou o cadastro. <br />
+          <br /> 3. COMPARTILHAMENTO E TRATAMENTO DE INFORMAÇÕES PESSOAIS O
+          Clube Benefit não disponibilizará Dados Pessoais coletados em seu site
+          sem conhecimento e expresso consentimento do Titular dos mesmos,
+          somente poderá compartilhar, nas seguintes situações e nos limites
+          exigidos e autorizados pela Lei: I. Com os seus clientes e parceiros
+          quando necessário e/ou apropriado à prestação de serviços relacionados
+          e em benefício do Titular; II. Com as empresas e indivíduos
+          contratados para a execução de determinadas atividades e serviços em
+          nome do Clube Benefit; III. Com fornecedores e parceiros para
+          consecução dos serviços contratados com o Clube Benefit; IV. Para
+          propósitos administrativos como: pesquisa, planejamento,
+          desenvolvimento de serviços, segurança e gerenciamento de risco. V.
+          Quando necessário em decorrência de obrigação legal, determinação de
+          autoridade competente, ou decisão judicial. <br />
+          <br /> 4. MOTIVOS LEGAIS PARA A DIVULGAÇÃO DE SEUS DADOS Em certas
+          circunstâncias, o Clube Benefit poderá divulgar Dados Pessoais, na
+          medida necessária ou apropriada, para órgãos governamentais,
+          consultores e outros terceiros com o objetivo de cumprir a legislação
+          ou ordem e intimação judicial, conforme previsto na LGPD: I. Cumprir
+          com uma legislação que exija tal divulgação; II. Investigar, impedir
+          ou tomar medidas relacionadas a atividades ilegais suspeitas ou reais
+          ou para cooperar com órgãos públicos ou para proteger a segurança
+          nacional; III. Execução de seus contratos; IV. Investigar e se
+          defender contra quaisquer reivindicações ou alegações de terceiros;
+          Página 3 de 2 V. Proteger a segurança ou a integridade dos serviços;
+          VI. Exercer ou proteger os direitos, a propriedade e a segurança do
+          Clube Benefit. VII. Em caso de venda, compra, fusão, reorganização,
+          liquidação ou dissolução do Clube Benefit. O Clube Benefit comunicará
+          os respectivos Titulares sobre eventuais demandas legais que resultem
+          na divulgação de informações pessoais, nos termos do que foi exposto
+          no item 4, a menos que tal comunicação seja vedada por lei ou proibida
+          por mandado judicial. <br />
+          <br /> 5. SEGURANÇA DE INFORMAÇÕES PESSOAIS Os Dados Pessoais
+          coletados serão armazenados em nuvem por meio de empresa contratada
+          pelo Clube Benefit, em atenção à legislação de dados vigente. O Clube
+          Benefit, juntamente com os seus parceiros e fornecedores,
+          procedimentos técnicos e administrativos de segurança para proteger a
+          confidencialidade, segurança e integridade de seus Dados Pessoais,
+          sempre com o intuito de evitar a ocorrência de eventuais danos ou
+          vazamentos, mesmo assim não há garantia de que as informações não
+          poderão ser acessadas, divulgadas, alteradas ou destruídas por
+          violação de qualquer uma das proteções físicas, técnicas ou
+          administrativas. <br />
+          <br /> 6. RETENÇÃO DE DADOS Nós mantemos os seus dados pessoais pelo
+          tempo legal permitido, enquanto houver prestação de serviços entre o
+          Escritório e Titulares ou a requerimento das autoridades. <br />
+          <br /> 7. BASES LEGAIS PARA PROCESSAMENTO O Clube Benefit apenas trata
+          Dados Pessoais em situações em que está autorizado legalmente ou
+          mediante o expresso e inequívoco consentimento do Titular. O Titular
+          tem o direito de negar ou retirar o consentimento fornecido ao Clube
+          Benefit, quando esta for a base legal para tratamento dos dados
+          pessoais. Caso tenha dúvidas sobre as bases legais para coleta,
+          tratamento e armazenamento de seus dados pessoais, entre em contato
+          com o Clube Benefit por meio do Encarregado de Dados (DPO) pelo e-mail
+          comercial@clubebenefit.com.br <br />
+          <br /> 8. DIREITO DE ACESSAR E CONTROLAR SEUS DADOS PESSOAIS O Clube
+          Benefit oferece as seguintes opções ao Titular, com base na LGPD: A
+          LGPD garante ao titular de dados pessoais os seguintes direitos: I.
+          Excluir dados: o Titular pode solicitar a exclusão de alguns dos seus
+          Dados Pessoais (se eles não são mais necessários para lhe fornecer os
+          serviços). Página 4 de 2 II. Alterar ou corrigir dados: o Titular pode
+          editar ou solicitar a edição de alguns dos seus Dados Pessoais, também
+          pode solicitar atualizações, alterações ou correções de seus dados em
+          determinados casos, principalmente se eles estiverem incorretos. III.
+          Colocar objeções, limites ou restrições ao uso de dados: o Titular
+          pode solicitar a interrupção do uso de todos ou alguns de seus Dados
+          Pessoais (se não tivermos o direito de continuar a usá-los), ou
+          limitar a nossa utilização de tais dados, destacando-se que o Clube
+          Benefit poderá tratar os Dados Pessoais de acordo com as bases legais.
+          IV. O Titular tem direito de acessar ou levar seus dados: o Titular
+          pode solicitar uma cópia dos seus Dados Pessoais em um formato legível
+          sob a forma impressa ou por meio eletrônico. O Titular poderá fazer as
+          solicitações listadas acima em conformidade com as leis aplicáveis,
+          entrando em contato por meio do e-mail comercial@clubebenefit.com.br
+          <br />
+          <br />
+          9. REVISÕES AO AVISO DE PRIVACIDADE Sempre que necessário, o Clube
+          Benefit atualizará este AVISO de Privacidade, o qual estará disponível
+          no site do Clube Benefit. Este AVISO entra em vigor a partir de sua
+          publicação. 10. CONTATO O Clube Benefit nomeou Vitor Kuster de Abreu
+          como Encarregado de Dados (“DPO”). O Titular pode entrar em contato
+          por meio do e-mail comercial@clubebenefit.com.br 11. FORO Este AVISO
+          está sujeito à Lei da República Federativa do Brasil e o Foro da
+          Comarca de Curitiba é competente para dirimir qualquer controvérsia
+          com relação à mesma.
+        </p>
+      </div>
+    </>
+  );
+}
